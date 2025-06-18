3c90c7165af639fa35099831c79919cc1e633076
JoaoAguiar19 <joao.gabriel.de.aguiar@ccc.ufcg.edu.br>
2025-06-18 09:41:54 -0300

    Finalizado

--- RR3-01-Roteiro-Revisao-JUnit-environment/src/main/java/vetor/Vetor.java
+++ RR3-01-Roteiro-Revisao-JUnit-environment/src/main/java/vetor/Vetor.java
@@ -9,10 +9,10 @@ import java.util.Comparator;
  * @author Adalberto
  *
  */
-public class Vetor {
+public class Vetor<V extends Comparable<V>> {
 
 	// O array interno onde os objetos manipulados são guardados
-	private Object[] arrayInterno;
+	private V[] arrayInterno;
 
 	// O tamanho que o array interno terá
 	private int tamanho;
@@ -21,46 +21,116 @@ public class Vetor {
 	private int indice;
 
 	// O Comparators a serem utilizados
-	private Comparator comparadorMaximo;
-	private Comparator comparadorMinimo;
+	private Comparator<V> comparadorMaximo;
+	private Comparator<V> comparadorMinimo;
 
 	public Vetor(int tamanho) {
 		super();
 		this.tamanho = tamanho;
-		this.indice = 0;
+		this.indice = -1;
+		this.arrayInterno = (V[]) new Comparable[tamanho];
 	}
 
-	public void setComparadorMaximo(Comparator comparadorMaximo) {
+	public void setComparadorMaximo(Comparator<V> comparadorMaximo) {
 		this.comparadorMaximo = comparadorMaximo;
 	}
 
-	public void setComparadorMinimo(Comparator comparadorMinimo) {
+	public void setComparadorMinimo(Comparator<V> comparadorMinimo) {
 		this.comparadorMinimo = comparadorMinimo;
 	}
 
 	// Insere um objeto no vetor
-	public void inserir(Object o) {
-
+	public void inserir(V o) {
+		if (!isCheio()) {
+			arrayInterno[++indice] = o;
+		}
 	}
 
 	// Remove um objeto do vetor
-	public Object remover() {
-		return null;
+	public V remover(V o) {
+		V result = procurar(o);
+
+		for (int i = 0; i <= arrayInterno.length; i++) {
+
+			if (result == null) {
+				break;
+			}
+
+			if (arrayInterno[i] != null && arrayInterno[i].equals(o)) {
+				arrayInterno[i] = arrayInterno[indice];
+				arrayInterno[indice] = null;
+				indice--;
+				break;
+			}
+		}
+
+		return result;
 	}
 
 	// Procura um elemento no vetor
-	public Object procurar(Object o) {
-		return null;
+	public V procurar(V o) {
+		V result = null;
+
+		for (int i = 0; i <= indice; i++) {
+			if (arrayInterno[i].equals(o)) {
+				result = arrayInterno[i];
+			}
+		}
+
+		return result;
 	}
 
 	// Diz se o vetor está vazio
 	public boolean isVazio() {
-		return false;
+		return indice == -1;
 	}
 
 	// Diz se o vetor está cheio
 	public boolean isCheio() {
-		return false;
+		return indice == tamanho - 1;
 	}
 
+	public V maximo() {
+		V max = null;
+		if (!isVazio()) {
+			max = arrayInterno[0];
+			for (int i = 0; i <= indice; i++) {
+				if (comparadorMaximo.compare(arrayInterno[i], max) > 0) {
+					max = arrayInterno[i];
+				}
+			}
+		}
+
+		return max;
+	}
+
+	public V minimo() {
+		V min = null;
+		if (!isVazio()) {
+			min = arrayInterno[0];
+			for (int i = 0; i <= indice; i++) {
+				if (comparadorMinimo.compare(arrayInterno[i], min) < 0) {
+					min = arrayInterno[i];
+				}
+			}
+		}
+
+		return min;
+	}
+
+}
+
+
+class ComparadorMaximo implements Comparator<Aluno> {
+	@Override
+	public int compare(Aluno o1, Aluno o2) {
+		return Double.compare(o1.getMedia(), o2.getMedia());
+	}
+}
+
+class ComparadorMinimo implements Comparator<Aluno> {
+	@Override
+	public int compare(Aluno o1, Aluno o2) {
+		return Double.compare(o1.getMedia(), o2.getMedia());
+	}
 }
