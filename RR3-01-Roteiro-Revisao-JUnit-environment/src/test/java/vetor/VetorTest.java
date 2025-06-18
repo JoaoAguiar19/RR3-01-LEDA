6022984bde657c5f82ac4880bbb9f2f542a94ea3
JoaoAguiar19 <joao.gabriel.de.aguiar@ccc.ufcg.edu.br>
2025-06-18 07:28:37 -0300

    Estruturacao

---
+++ RR3-01-Roteiro-Revisao-JUnit-environment/src/test/java/vetor/VetorTest.java
@@ -0,0 +1,40 @@
+package vetor;
+
+import org.junit.Test;
+
+public class VetorTest {
+    @Test
+    public void testInserir() {
+
+    }
+
+    @Test
+    public void testIsCheio() {
+
+    }
+
+    @Test
+    public void testIsVazio() {
+
+    }
+
+    @Test
+    public void testProcurar() {
+
+    }
+
+    @Test
+    public void testRemover() {
+
+    }
+
+    @Test
+    public void testSetComparadorMaximo() {
+
+    }
+
+    @Test
+    public void testSetComparadorMinimo() {
+
+    }
+}
